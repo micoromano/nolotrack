f3783f6222b0ab3babd63e61710c101a2d4a30d7
micoromano <marco.camelin@gmail.com>
2026-05-12 10:45:01 +0200

    release su vercel

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- output: 'export',
-  basePath: '/nolotrack',
+  /* config options here */
 };
 
 export default nextConfig;
